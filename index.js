92275ded33b82f2fb779a92024540591638d3fa1
DaisyS22 <rogelioumbay.ru@gmail.com>
2023-01-06 12:29:45 +0800

    typescript exercise

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+class City {
+    constructor(cityName, country, population) {
+        this.cityName = cityName;
+        this.country = country;
+        this.population = population;
+    }
+}
+const cities = [];
+function addCity(city) {
+    cities.push(city);
+}
+function renderList(cities) {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+    for (const city of cities) {
+        const li = document.createElement("li");
+        li.textContent = `${city.cityName}, ${city.country}, ${city.population}`;
+        list.appendChild(li);
+    }
+}
+const addForm = document.getElementById("addForm");
+addForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityName = document.getElementById("cityName")
+        .value;
+    const country = document.getElementById("country")
+        .value;
+    const population = document.getElementById("population")
+        .valueAsNumber;
+    addCity(new City(cityName, country, population));
+    renderList(cities);
+});
+const searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("input", () => {
+    const searchValue = searchInput.value.toLowerCase();
+    const filteredCities = cities.filter((city) => {
+        return (city.cityName.toLowerCase().includes(searchValue) ||
+            city.country.toLowerCase().includes(searchValue));
+    });
+    renderList(filteredCities);
+});
---
+++ index.ts
@@ -0,0 +1,51 @@
+class City {
+  cityName: string;
+  country: string;
+  population: number;
+  constructor(cityName: string, country: string, population: number) {
+    this.cityName = cityName;
+    this.country = country;
+    this.population = population;
+  }
+}
+
+const cities: City[] = [];
+
+function addCity(city: City) {
+  cities.push(city);
+}
+
+function renderList(cities: City[]) {
+  const list = document.getElementById("list") as HTMLElement;
+  list.innerHTML = "";
+  for (const city of cities) {
+    const li = document.createElement("li");
+    li.textContent = `${city.cityName}, ${city.country}, ${city.population}`;
+    list.appendChild(li);
+  }
+}
+
+const addForm = document.getElementById("addForm") as HTMLElement;
+addForm.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+  const cityName = (document.getElementById("cityName") as HTMLInputElement)
+    .value;
+  const country = (document.getElementById("country") as HTMLInputElement)
+    .value;
+  const population = (document.getElementById("population") as HTMLInputElement)
+    .valueAsNumber;
+  addCity(new City(cityName, country, population));
+  renderList(cities);
+});
+
+const searchInput = document.getElementById("searchInput") as HTMLInputElement;
+searchInput.addEventListener("input", () => {
+  const searchValue = searchInput.value.toLowerCase();
+  const filteredCities = cities.filter((city) => {
+    return (
+      city.cityName.toLowerCase().includes(searchValue) ||
+      city.country.toLowerCase().includes(searchValue)
+    );
+  });
+  renderList(filteredCities);
+});
